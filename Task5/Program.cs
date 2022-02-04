1c643f8aa380973ee5218b23f64e10fa7f46dcc3
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-04 15:54:26 +0300

    feat: Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task5
+{
+
+    class Program
+    {
+        private delegate double calc(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            calc calculator = (int a, int b, int c) => ((double)a + b + c) / 3;
+
+            Console.WriteLine(calculator(4, 3, 6));
+        }
+    }
+}
