fb055db15b5ad42cb00951616227721ea212ac37
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-04 15:49:48 +0300

    feat: Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        private delegate int delegate1(Random rnd);
+        private delegate double delegate2(delegate1[] arr);
+        static void Main(string[] args)
+        {
+            delegate1[] arr = new delegate1[10];
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = (rnd) => rnd.Next(0, 100);
+            }
+
+            delegate2 delegate2 = (arr) =>
+            {
+                Random rnd = new();
+                int total = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    total += arr[i](rnd);
+                }
+                return total / (double)arr.Length;
+            };
+
+            Console.WriteLine(delegate2(arr));
+        }
+    }
+}
